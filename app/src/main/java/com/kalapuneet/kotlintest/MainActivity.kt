dee493e15c9bb3be0a4725b2932750a83e4e12cb
Puneet Kala <puneetkkala@gmail.com>
2018-03-25 15:08:38 +0530

    Trying to fix the guard class

--- app/src/main/java/com/kalapuneet/kotlintest/MainActivity.kt
+++ app/src/main/java/com/kalapuneet/kotlintest/MainActivity.kt
@@ -2,11 +2,22 @@ package com.kalapuneet.kotlintest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.kalapuneet.kme.guard
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        guard{
+            failure {
+                Log.e("MainActivity", "Failure Block")
+            }
+            always {
+                Log.e("MainActivity", "Always Block")
+            }
+            Log.e("MainActivity", "Guard Block")
+        }
     }
 }
---
+++ app/src/main/java/com/kalapuneet/kotlintest/examples/GuardTest.kt
@@ -0,0 +1,9 @@
+package com.kalapuneet.kotlintest.examples
+
+class GuardTest {
+
+    @Throws(Exception::class)
+    fun exceptionCreator() {
+        throw Exception()
+    }
+}
